84e18d9ee5e8670b718721f5d6e603123521da5c
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-12-12 14:39:23 +0100

    Promotion relation

--- src/main/java/org/generation/italy/demo/SpringLaMiaPizzeriaCrudApplication.java
+++ src/main/java/org/generation/italy/demo/SpringLaMiaPizzeriaCrudApplication.java
@@ -1,11 +1,16 @@
 package org.generation.italy.demo;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 
 import org.generation.italy.demo.pojo.Drink;
 import org.generation.italy.demo.pojo.Pizza;
+import org.generation.italy.demo.pojo.Promotion;
 import org.generation.italy.demo.serv.DrinkServ;
 import org.generation.italy.demo.serv.PizzaServ;
+import org.generation.italy.demo.serv.PromotionServ;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -20,23 +25,65 @@ public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner {
 	@Autowired
 	private DrinkServ drinkServ;
 	
+	@Autowired
+	private PromotionServ promoServ;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringLaMiaPizzeriaCrudApplication.class, args);
 	}
 	
 	@Override
 	public void run(String... args) throws Exception {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy").withLocale(Locale.ITALY);
 		
-		Pizza p1 = new Pizza("Margherita", "é una margherita", 5);
-		Pizza p2 = new Pizza("Rustica", "patatine e salsiccia", 7);
-		Pizza p3 = new Pizza("Siciliana", "margherita con melanzane", 8);
+		LocalDate startDateProm1 = LocalDate.of(2023, 6, 13);
+		LocalDate endDateProm1 = LocalDate.of(2023, 8, 23);
+		Promotion pro1 = new Promotion("Promo1", startDateProm1, endDateProm1);
+		
+		LocalDate startDateProm2 = LocalDate.of(2022, 2, 15);
+		LocalDate endDateProm2 = LocalDate.of(2024, 8, 23);
+		Promotion pro2 = new Promotion("Promo2", startDateProm2, endDateProm2);
+		
+		LocalDate startDateProm3 = LocalDate.of(2024, 6, 13);
+		LocalDate endDateProm3 = LocalDate.of(2026, 8, 23);
+		Promotion pro3 = new Promotion("Promo3", startDateProm3, endDateProm3);
+		
+		promoServ.save(pro1);
+		promoServ.save(pro2);
+		promoServ.save(pro3);
+		
+		System.out.println("-----------------------------------------------------------");
+		
+		Pizza p1 = new Pizza("Margherita", "é una margherita", 5, pro1);
+		Pizza p2 = new Pizza("Rustica", "patatine e salsiccia", 7, pro1);
+		Pizza p3 = new Pizza("Siciliana", "margherita con melanzane", 8, pro3);
 		
 		pizzaServ.save(p1);
 		pizzaServ.save(p2);
 		pizzaServ.save(p3);
 		
 		List<Pizza> pizzas = pizzaServ.all();
-		System.err.println(pizzas);
+		//System.err.println(pizzas);
+		
+		System.out.println("-----------------------------------------------------------");
+		
+		List<Promotion> promotions = promoServ.allWPizzas();
+		for (Promotion promotion : promotions) {
+			System.err.println(promotion.getPizzas());
+		}
+		
+		promoServ.deleteById(1);
+		
+		promotions = promoServ.allWPizzas();
+		for (Promotion promotion : promotions) {
+			System.err.println(promotion.getPizzas());
+		}
+		System.out.println();
+		pizzas = pizzaServ.all();
+		for (Pizza pizza : pizzas) {
+			System.err.println(pizza);
+		}
+		System.out.println("-----------------------------------------------------------");
 		
 		Drink d1 = new Drink("LosPollos", "eso un pollos", 10000);
 		Drink d2 = new Drink("ChicaMala", "eso una chica malissima", 1);
@@ -47,6 +94,6 @@ public class SpringLaMiaPizzeriaCrudApplication implements CommandLineRunner {
 		drinkServ.save(d3);
 		
 		List<Drink> drinks = drinkServ.all();
-		System.err.println(drinks);
+		//System.err.println(drinks);
 	}
 }
--- src/main/java/org/generation/italy/demo/pojo/Pizza.java
+++ src/main/java/org/generation/italy/demo/pojo/Pizza.java
@@ -7,7 +7,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
@@ -33,11 +35,16 @@ public class Pizza implements PriceableInt{
 	@Min(value = 0)
 	private int price;
 	
+	@ManyToOne
+	@JoinColumn(name = "promotion_id", nullable = false)
+	private Promotion promotion;
+	
 	public Pizza() {}
-	public Pizza(String name, String description, int price) {
+	public Pizza(String name, String description, int price, Promotion promotion) {
 		setName(name);
 		setPrice(price);
 		setDescription(description);
+		setPromotion(promotion);
 	}
 	
 	
@@ -66,9 +73,15 @@ public class Pizza implements PriceableInt{
 		this.price = price;
 	}
 	
+	public Promotion getPromotion() {
+		return promotion;
+	}
+	public void setPromotion(Promotion promotion) {
+		this.promotion = promotion;
+	}
 	@Override
 	public String toString() {
-		return id + " - " + name + "\n" + description + "\n" + price;
+		return id + " - " + name + "\n" + description + "\n" + price + "\n";
 	}
 	
 	
